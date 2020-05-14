bfc06429292e32d378c1817ef0249648d4dfba0f
Villegas Ramirez Luis Eduardo <luis.villegas17@tectijuana.edu.mx>
2020-05-13 19:46:42 -0700

    P04

---
+++ ControlInventario/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControlInventario.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+              return  RedirectToAction("Index","Principal");
+            }
+            
+             return View();
+            
+            
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Inventarios Tec.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contacto";
+
+            return View();
+        }
+    }
+}
